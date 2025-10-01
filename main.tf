c49bf01fd6e25e6076e2342ffab5867f42beaf2d
wielblad <38068695+wielblad@users.noreply.github.com>
2025-10-01 17:23:00 +0000

    pg

--- main.tf
+++ main.tf
@@ -63,7 +63,6 @@ resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_all" {
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "255.255.255.255"
 }
-}
 
 resource "azurerm_postgresql_flexible_server_database" "usersdb" {
   name      = "usersdb"
