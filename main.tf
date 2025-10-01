f928dd5ae34e017b7107089e3fc902b084e6a593
wielblad <38068695+wielblad@users.noreply.github.com>
2025-10-01 12:30:52 +0000

    rabbit

--- main.tf
+++ main.tf
@@ -35,3 +35,42 @@ resource "azurerm_linux_web_app" "example" {
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountuser04"
+  resource_group_name      = "rg-04"
+  location                 = "westeurope"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_container_group" "rabbitmq" {
+  name                = "rabbitmq-group"
+  location            = "westeurope"
+  resource_group_name = "rg-04"
+  os_type             = "Linux"
+
+  container {
+    name   = "rabbitmq"
+    image  = "rabbitmq:3-management"
+    cpu    = "1.0"
+    memory = "1.5"
+
+    ports {
+      port     = 5672
+      protocol = "TCP"
+    }
+    ports {
+      port     = 15672
+      protocol = "TCP"
+    }
+    environment_variables = {
+      RABBITMQ_DEFAULT_USER = "admin"
+      RABBITMQ_DEFAULT_PASS = "adminpassword"
+    }
+  }
+
+  ip_address_type = "Public"
+  dns_name_label  = "rabbitmquser04"
+}
